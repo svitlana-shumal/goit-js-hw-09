3a1a6f6d2f490340b7f78f4aa2365812de045692
Svitlana Shumal <svetlankachajka@gmail.com>
2025-05-28 13:16:36 +0300

    added code

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,11 +1,37 @@
-// Оголоси поза будь-якими функціями об’єкт formData з полями email та message, які спочатку мають порожні рядки як значення:
-// { email: "", message: "" }.
-// Використовуй метод делегування для відстеження змін у формі через подію input.
-// Зберігай актуальні дані з полів email та message у formData та записуй цей об’єкт у локальне сховище.
-// Використовуй ключ "feedback-form-state" для зберігання даних у сховищі.
-// При завантаженні сторінки перевір, чи є дані у локальному сховищі.
-// Якщо так, використовуй їх для заповнення форми та об'єкта formData. Якщо ні, залиш поля форми порожніми.
-// Перед відправленням форми переконайся, що обидва поля форми заповнені.
-// Якщо будь-яке з полів (властивостей об’єкта formData) порожнє, показуй сповіщення з текстом
-// «Fill please all fields».
-// Якщо всі поля заповнені, виведи у консоль об’єкт formData з актуальними значеннями, очисти локальне сховище, об’єкт formData і поля форми.
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {
+  email: '',
+  message: '',
+};
+
+form.addEventListener('input', event => {
+  const { name, value } = event.target;
+  formData[name] = value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const dataStorage = localStorage.getItem(STORAGE_KEY);
+  if (dataStorage) {
+    const dataParse = JSON.parse(dataStorage);
+    formData.email = dataParse.email;
+    formData.message = dataParse.message;
+  }
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  if (!formData.email.trim() || !formData.message.trim()) {
+    alert('Fill please all fields');
+    return;
+  }
+  console.log(formData);
+
+  formData.email = '';
+  formData.message = '';
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+});
